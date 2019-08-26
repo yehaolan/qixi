ecdd4ccd07e8a5262ee402a78145f4b64b769213
Haolan Ye <yehaolan@gmail.com>
2019-08-25 21:16:40 -0700

    Updates

---
+++ precache-manifest.7f40b49e745a14a348dfc1a0213f823c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5893872eb72b9597f6a8da441c87319",
+    "url": "/qixi/index.html"
+  },
+  {
+    "revision": "1610ae7a1bd8b4ed5cf5",
+    "url": "/qixi/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "b0557a420737a47474df",
+    "url": "/qixi/static/js/2.f36d47fb.chunk.js"
+  },
+  {
+    "revision": "1610ae7a1bd8b4ed5cf5",
+    "url": "/qixi/static/js/main.78b508c9.chunk.js"
+  },
+  {
+    "revision": "042e724182ad74d372a3",
+    "url": "/qixi/static/js/runtime~main.13927a5f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/qixi/static/media/logo.5d5d9eef.svg"
+  }
+]);
